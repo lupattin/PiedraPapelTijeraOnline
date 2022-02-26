6e5b980e70f4bf763015eca4cd4b167012e3d517
Luca <lupattin@gmail.comgit config --global user.email lupattin@gmail.comgit config --global user.email lupattin@gmail.com>
2022-02-26 20:17:15 -0300

    intentando deployar

--- server/index.ts
+++ server/index.ts
@@ -9,12 +9,13 @@ const app = express()
 /* Inicio middlewares */
 app.use(cors());
 app.use(express.json());
+app.use(express.static('dist'));
 
 /* USERS COLLECIONS y PORT */
 const usersCollection = baseDeDatos.collection("users")
 const roomsCollection = baseDeDatos.collection("rooms")
 const roomsRtdbRef = realtimeDatabase.ref("rooms")
-const port = 3000
+const port = process.env.PORT || 3000
 
 /* RUTAS GET */
 
@@ -35,73 +36,76 @@ app.get('/room/:roomid/:username', (req, res) => {
       roomRef.update({
         invited: userName,
         invitedonline: true
-       })
-      }
-    })
+      })
+    }
   })
-  
-  /* RUTAS POST */
-  
-  /* Actualiza la jugada del jugador */
+})
+
+/* RUTAS POST */
+
+/* Actualiza la jugada del jugador */
 app.post('/room/:roomid/:username/:play', (req, res) => {
   const roomid = req.params.roomid  
   const userName = req.params.username  
   const userPlay = req.params.play  
   
-      const roomRef = realtimeDatabase.ref("rooms/" + roomid)
-      roomRef.get().then((snap)=>{
-        const data = snap.val()
-          if(userName == data.owner){
-            roomRef.update({
-              ownerplay: userPlay,
+  const roomRef = realtimeDatabase.ref("rooms/" + roomid)
+  roomRef.get().then((snap)=>{
+    const data = snap.val()
+    if(userName == data.owner){
+      roomRef.update({
+        ownerplay: userPlay,
              }).then(()=>{
-              roomRef.get().then((snap)=>{
-                const dataNueva = snap.val()
-                res.send(dataNueva)
+               roomRef.get().then((snap)=>{
+                 const dataNueva = snap.val()
+                 res.send(dataNueva)
+                })
               })
-             })
-          }else if(userName == data.invited){
-            roomRef.update({
-              invitedplay: userPlay,
-             }).then(()=>{
-              roomRef.get().then((snap)=>{
-                const dataNueva = snap.val()
-                res.send(dataNueva)
+            }else if(userName == data.invited){
+              roomRef.update({
+                invitedplay: userPlay,
+              }).then(()=>{
+                roomRef.get().then((snap)=>{
+                  const dataNueva = snap.val()
+                  res.send(dataNueva)
+                })
               })
-             })
-          }
-      })
-})
-app.post('/user', function (req, res) {
-    const newUserDoc = usersCollection.doc()
-    newUserDoc.create(req.body).then(()=>{
-      res.send(newUserDoc.id)
-    })
-    });
-
-/* Crea Room en firestore */
-app.post('/room', function (req, res) {
-    const newRoomDoc = roomsCollection.doc()
-    newRoomDoc.create(req.body).then(()=>{
-      res.send(newRoomDoc.id)
-    });
-  })
-/* Crea room en realtime Database */
-  app.post('/room/:roomid/:user', function (req, res) {
-    const roomid = req.params.roomid
-    const user = req.params.user
-    
-    roomsRtdbRef.update({
-      [roomid]:{
-        owner: user,
-        owneronline: true
-      }
-    }).then(()=>{
-      res.send("Room Created")
-    })
-  })
-
-/* La magian que ejecuta el puerto */
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+            }
+          })
+        })
+        app.post('/user', function (req, res) {
+          const newUserDoc = usersCollection.doc()
+          newUserDoc.create(req.body).then(()=>{
+            res.send(newUserDoc.id)
+          })
+        });
+        
+        /* Crea Room en firestore */
+        app.post('/room', function (req, res) {
+          const newRoomDoc = roomsCollection.doc()
+          newRoomDoc.create(req.body).then(()=>{
+            res.send(newRoomDoc.id)
+          });
+        })
+        /* Crea room en realtime Database */
+        app.post('/room/:roomid/:user', function (req, res) {
+          const roomid = req.params.roomid
+          const user = req.params.user
+          
+          roomsRtdbRef.update({
+            [roomid]:{
+              owner: user,
+              owneronline: true
+            }
+          }).then(()=>{
+            res.send("Room Created")
+          })
+        })
+        /* Ruta para heroku para SPA*/
+        app.get("*",( req, res ) =>{
+          res.send(__dirname + "/dist/index.html")
+        })
+        /* La magian que ejecuta el puerto */
+        app.listen(port, () => {
+          console.log(`Example app listening at http://localhost:${port}`)
+        })
